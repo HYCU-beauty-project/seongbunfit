c91af2599f0c856e905688d439560db099255d80
조창래 <chochanglae1223@gmail.com>
2026-07-22 05:38:55 +0900

    chore: remove unused files and dependencies

--- app/mobile/chat-intro/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-// /mobile/chat 안에 면책 동의 모달 이미 있어서
-// 별도 안내 화면 없이 바로 /mobile/chat으로 보냄
-export default function MobileChatIntroPage() {
-  redirect("/mobile/chat");
-}
